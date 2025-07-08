c549f6ab7c5202e3cce1bf1650d2fed9cb2a9221
Ebenezer Acquah <ebenezeracquah000@gmail.com>
2025-07-08 10:14:12 -0700

    forth commit

--- script.js
+++ script.js
@@ -5,7 +5,3 @@ const loginBtn = document.getElementById('login');
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");
 });
-
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
-});
